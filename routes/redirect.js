875764cf105de60d7673c16e3b693633b0aebb5f
Tom McTiernan <thomasmctiernan@gmail.com>
2016-05-24 09:30:22 +0100

    Add redirect functionality

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Redirect back to index, with query string parameter. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('index?token=exampleToken');
 });
 
 module.exports = router;
